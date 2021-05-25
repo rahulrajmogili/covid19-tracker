2789b8d8eaa0154486ff07049375d1738f7d2546
Rahul Raj Mogili <mogilirahulraj@gmail.com>
2021-05-25 12:18:38 -0400

    pulling in individual data for a specific country and putting it on the table

--- src/App.js
+++ src/App.js
@@ -1,49 +1,73 @@
-import './App.css';
-import { Card, CardContent, FormControl, MenuItem, Select } from '@material-ui/core';
-import { useEffect, useState } from 'react';
-import InfoBox from './InfoBox'
-import Map from './Map'
+import "./App.css";
+import {
+  Card,
+  CardContent,
+  FormControl,
+  MenuItem,
+  Select,
+} from "@material-ui/core";
+import { useEffect, useState } from "react";
+import InfoBox from "./InfoBox";
+import Map from "./Map";
 
 function App() {
-
   // STATE IS ESSENTIALLY HOW TO WRITE A VARIABLE IN REACT <<<<<<<<<<
-  
+
   // https://disease.sh/v3/covid-19/countries
 
   // USEEFFECT essentially runs a piece of code based on a given condition
 
-  const [countries, setCountries] = useState([])
-  const [country, setCountry] = useState('worldwide')
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState([]);
+
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
 
   useEffect(() => {
     // The code inside here runs only once when the component loads and not again!
     // async -> send a request to server, wait for it, and do something with that info
 
-    const getCountriesData = async() => {
-      await fetch('https://disease.sh/v3/covid-19/countries')
-      .then((response) => response.json())
-      .then((data) => {
-        const countries = data.map((country) => (
-          {
-            name: country.country,    // United Kingdom, United States
-            value: country.countryInfo.iso2 // UK, USA
-          }
-        ));
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country, // United Kingdom, United States
+            value: country.countryInfo.iso2, // UK, USA
+          }));
 
-        setCountries(countries);
-      })
-    }
+          setCountries(countries);
+        });
+    };
 
     getCountriesData();
   }, []);
 
-
   // We are using async here soon, not yet needed
-  const onCountryChange = async(event) => {
-    const countryCode = event.target.value
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
 
-    setCountry(countryCode)
-  }
+    const url =
+      countryCode === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        // We are setting the country code here
+        setCountry(countryCode);
+
+        // We are setting the country info in our app
+        setCountryInfo(data);
+      });
+  };
 
   return (
     <div className="app">
@@ -58,24 +82,33 @@ function App() {
               onChange={onCountryChange}
             >
               <MenuItem value="worldwide">Worldwide</MenuItem>
-              {
-                countries.map(country => (
-                  <MenuItem value={country.value}>{country.name}</MenuItem>
-                ))
-              }
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
             </Select>
           </FormControl>
         </div>
 
         <div className="app__stats">
-          <InfoBox title="Coronavirus Cases" cases={1234} total={2000}/>
-          
-          <InfoBox title="Coronavirus recoveries" cases={12341} total={500}/>
-          
-          <InfoBox title="Coronavirus deaths" cases={41232} total={80}/>
-          
+          <InfoBox
+            title="Coronavirus cases"
+            cases={countryInfo.todayCases}
+            total={countryInfo.cases}
+          />
+
+          <InfoBox
+            title="Coronavirus recoveries"
+            cases={countryInfo.todayRecovered}
+            total={countryInfo.recovered}
+          />
+
+          <InfoBox
+            title="Coronavirus deaths"
+            cases={countryInfo.todayDeaths}
+            total={countryInfo.deaths}
+          />
         </div>
-        <Map/>
+        <Map />
       </div>
 
       <Card className="app__right">
